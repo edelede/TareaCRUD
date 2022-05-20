3deaf5baac0b7ff92cf0107c3307dc9f5842f33f
Luis Vasquez Fica <levasquez.-@hotmail.com>
2022-05-20 14:07:55 -0400

    update agregado

--- Listar/update.php
+++ Listar/update.php
@@ -7,29 +7,31 @@
     while($fila = mysqli_fetch_assoc($resultado)){
 
 ?>
-    <form>        
+    <form action="update.php" method="POST">        
         <label for="">ID</label>
         <input type="text" name="txtid" placeholder="id" value="<?=$fila['id']?>"><br>
         <label for="">URL</label>
         <input type="text" name="txturl" placeholder="imagen" value="<?=$fila['url']?>"><br>
-        <input type="submit" value="Actualizar"><a href="../index.php">Cancelar</a>
+        <input type="submit" name="Actualizar" value="Actualizar"><a href="../index.php">Cancelar</a>
     </form>
 
     <?php 
     } ?>
 
 <?php
-    if(isset($_GET['submit'])){
-        $idp = $_GET['txtid'];
-        $urlp = $_GET['txturl'];
+    if(isset($_POST['Actualizar'])){
+        $idp = $_POST['txtid'];
+        $urlp = $_POST['txturl'];
         if($idp!=null || $urlp!=null){
             $consulta2 = "UPDATE imagenes SET id='$idp', url='$urlp' WHERE id = '$idp'";
             $resultado2 = mysqli_query($con,$consulta2);
             if($resultado2){
                 header("Location: read.php");
+            }else{
+                echo "NO SE PUDO ACTUALIZAR";
             }
         }
-    }    
+    }
         
         
         
